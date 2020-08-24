79bff2f73b4daba087848c93b9b1157b8fbec91d
Artem <qvazimodo@mail.ru>
2020-08-24 23:13:22 +0300

    lesson-2

--- script.js
+++ script.js
@@ -1,24 +1,133 @@
 const goods = [
-  { titleImage: "img/prod_detals1.png", title: 'Mango T-shirt1', color: "Red", size: "XLL", unitPrice: "150", totalQuantity: "3", shipping: "FREE"   },
-  { titleImage: "img/prod_detals2.png", title: 'Mango T-shirt2', color: "Black", size: "XL", unitPrice: "200", totalQuantity: "4", shipping: "FREE"  },
-  { titleImage: "img/prod_detals3.png", title: 'Mango T-shirt3', color: "Red", size: "XLL", unitPrice: "150", totalQuantity: "6", shipping: "FREE"   },
-  
-];
+    { titleImage: "img/catalog_items_1.png", title: 'Mango T-shirt1',   color: "Red", size: "XLL", unitPrice: "10",  shipping: "FREE"   },
+    { titleImage: "img/catalog_items_2.png", title: 'Mango T-shirt2', color: "Black", size: "XL", unitPrice: "20",  shipping: "FREE"  },
+    { titleImage: "img/catalog_items_3.png", title: 'Mango T-shirt3', color: "Red", size: "XLL", unitPrice: "30",  shipping: "FREE"   },
+    { titleImage: "img/catalog_items_4.png", title: 'Mango T-shirt4', color: "Red", size: "XLL", unitPrice: "40",  shipping: "FREE"   },
+    { titleImage: "img/catalog_items_5.png", title: 'Mango T-shirt5', color: "Black", size: "XL", unitPrice: "50",  shipping: "FREE"  },
+    { titleImage: "img/catalog_items_6.png", title: 'Mango T-shirt6', color: "Red", size: "XLL", unitPrice: "60",  shipping: "FREE"   },
+    { titleImage: "img/catalog_items_7.png", title: 'Mango T-shirt7', color: "Red", size: "XLL", unitPrice: "70",  shipping: "FREE"   },
+    { titleImage: "img/catalog_items_8.png", title: 'Mango T-shirt8', color: "Black", size: "XL", unitPrice: "80",  shipping: "FREE"  },
+    { titleImage: "img/catalog_items_9.png", title: 'Mango T-shirt9', color: "Red", size: "XLL", unitPrice: "90",  shipping: "FREE"   },
+    
+  ];
 
+class GoodsItem {
 
+  constructor(titleImage = "def", title  = "def", color = "def", size = "def", unitPrice = "def", shipping = "def") {
+    this.titleImage = titleImage;
+    this.title = title;
+    this.color = color;
+    this.size = size;
+    this.unitPrice = unitPrice;
+    this.shipping = shipping;
 
-
-
-const renderGoodsItem = (titleImage="", title="default", color="green", size="X", unitPrice="100", totalQuantity="0", shipping="FREE") => {
-    let sum = 0;
-    sum= parseInt(totalQuantity) * parseInt(unitPrice);
-    return  `<br><div class="shopping_product"> <div class="prod_detals" style="width: 403px;"><img src=${titleImage} alt="prod_detals3"><div class="prod__detals"><a href="#"><h1>${title}</h1></a> <p>Color:<x> ${color}</x></p> <p>Size:<x> ${size}</x></p></div>  </div>  <div class="unit_price">$${unitPrice}</div> <div class="quantity"><input type="number" class="quantityNum" min="0" max="10" step="1" value= ${totalQuantity} style="height: 30px; width: 54px;"></div> <div class="shipping">${shipping}</div> <div class="subtotal"> $${sum}</div> <div class="action"><i class="fas fa-times-circle"></i></div></div>` ;
-};
-
- var renderGoodsList = (list) => {
-  var goodsList = list.map(item => renderGoodsItem(item.titleImage, item.title, item.color, item.size, item.unitPrice, item.totalQuantity, item.shipping));
-  document.querySelector('.shopping_productt').innerHTML = goodsList.join('');
+}
+render() {
+  return `
+          <div class="like_items_list " data-color = ${this.color} data-size = ${this.size} data-shipping = ${this.shipping}     >
+          <a href="#" class="like_items_link"><img class="items_img" src= ${this.titleImage}   ></a>
+          <div class="like_items_content">
+              <a href="#" class="like_items_name">${this.title}</a>
+              <div class="like_price_stars"> <p class="like_items_price" style="margin-top: 17px; margin-left: 1px;">$${this.unitPrice}</p>
+                  </div>
+          </div>
+          <a href="#" class="items_add"> <img src="img/cart_2.png" alt="" style="margin-right: 8px">Add to Cart</a>
+          </div>
+  `
+}
 }
 
-renderGoodsList(goods);
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchGoods() {
+    // асинхронная логика, получение с бека
+    this.goods = goods;
+  }
+  render() {
+    let html = '';
+    this.goods.forEach(({ titleImage, title, color, size, unitPrice, shipping }) => {
+      const goodItem = new GoodsItem(titleImage, title, color, size, unitPrice, shipping);
+      html += goodItem.render();
+    });
+    document.querySelector('.content_product_flex').innerHTML = html;
+  }
+  summ() {
+    // вычисление общей суммы элементов
+    let sum = 0;
+    this.goods.forEach(({unitPrice}) => {
+      if(!isNaN (parseInt(unitPrice))) {
+        sum += parseInt(unitPrice);
+        }
+      else {
+        console.log ("Error");
+        sum = 0;
+      }
+    })
+    console.log (sum);
+  }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.summ();
+
+
+
+// класс для элемента корзины
+class BasketItem {
+
+  constructor(titleImage , title, color, size, unitPrice, shipping) {
+    this.titleImage = titleImage;
+    this.title = title;
+    this.color = color;
+    this.size = size;
+    this.unitPrice = unitPrice;
+    this.shipping = shipping;
+
+}
+renderBascketItem() {
+  // формирование вставки на страницу корзины
+  return `<div class="shopping_product" style="margin-top: 26px;"> 
+  <div class="prod_detals" style="width: 403px;"> 
+      <img src=${this.titleImage} alt="prod_detals1">
+      <div class="prod__detals">
+          <a href="single_page.html"><h1>${this.title}</h1></a>
+          <p>Color:<x> ${this.color}</x></p>
+          <p>Size:<x> ${this.size}</x></p></div>            </div>
+  <div class="unit_price">$${this.unitPrice}</div>
+  <div class="quantity"><label>
+      <input  type="number" min="0" max="10" step="1" style="height: 30px; width: 54px;" >
+  </label></div>
+  <div class="shipping">${this.shipping}</div>
+  <div class="subtotal">$300</div>
+ <div class="action"><i class="fas fa-times-circle"></i></div></div>  
+    `
+}
+}
+// класс для корзины
+class BascketList {
+  constructor() {
+      // параметр - объект, выбранные продукты
+    this.bascketGoods = []; 
+  }
+  fetchBascketGoods() {
+    // получение выбранных на странице продуктов
+    this.bascketGoods = bascketGoods; 
+  }
+  renderBascketList() {  
+    // вставка продуктов на страницу корзины
+    // let html = '';
+    // this.bascketGoods.forEach(({ titleImage, title, color, size, unitPrice, shipping }) => {
+    //   const bascketItem = new BasketItem(titleImage, title, color, size, unitPrice, shipping);
+    //   html += bascketItem.renderBascketItem();
+    // });
+    // document.querySelector('.shopping_productt').innerHTML = html;
+  }
+  summBascket() {
+  //  вычисление общей суммы выбранных продуктов 
+  }
+}
 
